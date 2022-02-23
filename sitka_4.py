45bc026cc4384aabc861354f3b85488dc49ab72d
Riley Richards <riley_richards1@baylor.edu>
2022-02-23 13:19:39 -0600

    Added chart for death valley

---
+++ sitka_4.py
@@ -0,0 +1,77 @@
+#1) changing the file to include all the data for the year of 2018
+#2) change the title to  - Daily low and high temperatures - 2018
+#3) extract low temps from the file and add to chart
+#4) shade in the area between high and low
+
+
+import csv
+from datetime import datetime
+
+sitka_file = open("death_valley_2018_simple.csv",'r')
+
+sitka = csv.reader(sitka_file, delimiter = ',')
+
+header_row = next(sitka)
+
+print(type(header_row))
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+lows = []
+highs = []
+dates = []
+
+#test_date = datetime.strptime('2018-07-01', '%Y-%m-%d')
+#print(test_date)
+
+
+for row in sitka:
+    try:
+        current_date = datetime.strptime(row[2],'%Y-%m-%d')
+        high = int(row[4])
+        low = int(row[5])
+        
+        
+    except ValueError:
+        print(f"Missing data for {current_date}") 
+
+    else:    
+        highs.append(high)
+        lows.append(low)
+        dates.append(current_date)
+
+print(highs)
+print(lows)
+print(dates)
+
+import matplotlib.pyplot as plt
+
+figure = plt.figure()
+
+plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+plt.title("Daily low and high temperatures - 2018", fontsize=16)
+plt.xlabel("Month of July 2018")
+plt.ylabel("Temperatures (F)", fontsize=16)
+plt.tick_params(axis="both", which="major", labelsize=16)
+
+figure.autofmt_xdate()
+
+#plt.show()
+
+#subplots
+plt.subplot(2,1,1)
+plt.plot(dates,highs,c="red")
+plt.title("Highs")
+
+
+plt.subplot(2,1,2)
+plt.plot(dates,highs,c="blue")
+plt.title("Lows")
+
+plt.suptitle("Highs and Lows of Death Valley, California 2018")
+
+plt.show()
